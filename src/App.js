fa37666e30250d6cd4efb580054af617c2de3225
Kean <keanng@muvlabs.io>
2016-01-27 15:46:16 +0800

    0.0.2

--- src/App.js
+++ src/App.js
@@ -1,64 +1,199 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+//For Material UI Button
+import FlatButton from 'material-ui/lib/flat-button';
+import RaisedButton from 'material-ui/lib/raised-button';
+import FloatingActionButton from 'material-ui/lib/floating-action-button';
 
- var i = 0;
- 
-export default class App extends Component {
+//For Material UI TextField
+import TextField from 'material-ui/lib/text-field';
 
- clickMe(){
+//For Material UI Checkbox
+import Checkbox from 'material-ui/lib/checkbox';
+import RadioButton from 'material-ui/lib/radio-button';
+import RadioButtonGroup from 'material-ui/lib/radio-button-group';
+import Toggle from 'material-ui/lib/toggle'
 
-	 if(document.getElementById('txtInput').value !== '')
-	{
-	    i++;
-		
-		var title = document.getElementById('txtInput').value;
-		var node = document.createElement('div');
-		node.setAttribute("id", "div" + i);
-		node.innerHTML = '<input type="checkbox" id="chk' + i +'" name="chk' + i + '"><label for="lbl' + i + '" id="lbl' + i +'">' + title + '</label><input type="image" id="del' + i +'" name="del' + i + '" src="https://cdn4.iconfinder.com/data/icons/devine_icons/Black/PNG/Folder%20and%20Places/Trash-Recyclebin-Empty-Closed.png" name="image" width="18" height="18" />';
-		
-		document.getElementById('container').appendChild(node);
-		document.getElementById('txtInput').value = "";
+//For Material UI Title Bar
+import AppBar from 'material-ui/lib/app-bar';
 
-		//For line through on checked
-		var strike = document.getElementById('chk' + i);
-		strike.addEventListener('change', function(e){
-		e.preventDefault();
-		var rows = this.id.replace(/[^0-9]/g, '');
+//MUI Cards
+import Avatar from 'material-ui/lib/avatar';
+import Card from 'material-ui/lib/card/card';
+import CardActions from 'material-ui/lib/card/card-actions';
+import CardHeader from 'material-ui/lib/card/card-header';
+import CardMedia from 'material-ui/lib/card/card-media';
+import CardTitle from 'material-ui/lib/card/card-title';
+import CardText from 'material-ui/lib/card/card-text';
 
-		if(document.getElementById(this.id).checked){
-		document.getElementById('lbl' + rows).style.textDecoration='line-through';
-		}
-		else{
-		document.getElementById('lbl' + rows).style.textDecoration='none';
-		}
-		document.getElementById('txtInput').focus();
+import AppStyle from './styles/App';
+
+var App = React.createClass({
+
+	getInitialState() {
+		return { todos: [] }
+	},
+
+	generateId() {
+		return Math.floor(Math.random()*90000) + 10000
+	},
+
+	handleNodeRemoval(nodeId) {
+		var todos = this.state.todos
+
+		todos = todos.filter((el) => {
+			return el.id !== nodeId
 		});
 
-		//For delete function
-		var trash = document.getElementById('del' + i);
-		trash.addEventListener('click', function(e){
-		e.preventDefault();
-		var rows = this.id.replace(/[^0-9]/g, '');
-		var element = document.getElementById("div" + rows);
-		element.parentNode.removeChild(element);
-		document.getElementById('txtInput').focus();
-		});
-	}
-	else
-	{
-		alert('Input is empty');
-	}
-	document.getElementById('txtInput').focus();
-}
+		this.setState({todos})
+		return;
+	},
 
-render() {
-    return (
-	<div id='container'>
-      	<h1>YATL</h1>
-	<input type='text' id='txtInput' placeholder='I need to...' ></input>
-	<button id='btnAdd' onClick={this.clickMe} >+</button>
-	<br/>
-	<br/>
-      	</div>
-    )
-  }
-}
+	handleSubmit(task) {
+		var todos = this.state.todos
+		var id = this.generateId().toString()
+		var complete = 'false'
+		todos = todos.concat([{id, task, complete}])
+		this.setState({todos})
+
+		this.firebaseRef.push({
+			task: this.state.task
+		})
+		this.setState({task: ""})
+	},
+
+	handleToggleComplete(nodeId) {
+		var todos = this.state.todos
+		for (var i in todos) {
+			if (todos[i].id === nodeId) {
+				todos[i].complete = todos[i].complete === 'true' ? 'false' : 'true'
+				break
+			}
+		}
+		this.setState({todos})
+		return
+	},
+
+	componentWillMount() {
+		this.firebaseRef = new Firebase("https://ReactFireTodoApp.firebaseio.com/items/")
+		this.firebaseRef.on("child_added", function(dataSnapShot){
+			this.todos.push(dataSnapShot.val())
+			this.setState({todos:this.todos})
+		}.bind(this))
+	},
+
+    componentWillUnmount() {
+      	this.firebaseRef.off()
+    },
+
+	render() {
+		return (
+			<div className="well">
+				<AppBar title="YATL" iconClassNameRight="muidocs-icon-navigation-expand-more" />
+				<TodoForm onTaskSubmit={this.handleSubmit} />
+				<TodoList todos={this.state.todos} removeNode={this.handleNodeRemoval} toggleComplete={this.handleToggleComplete} />
+			</div>
+		)
+	},
+})
+
+var TodoForm = React.createClass({
+
+	handleClick(e) {
+		e.preventDefault()
+		this.addTodoItem()
+	},
+
+	handleKeyPress(e){
+		if(e.charCode==13){
+			this.addTodoItem()
+		}
+	},
+
+	addTodoItem(){
+		var task = this.task.getValue().trim()
+		if (!task) {
+			alert('Input is empty!!')
+			return
+		}
+		this.props.onTaskSubmit(task)
+		this.task.clearValue()
+		return
+	},
+
+	render() {
+		return (
+			<div>
+			   	<TextField type="text" id="task" ref={(ref) => this.task = ref} onKeyPress={this.handleKeyPress} placeholder="I need to..."  />	
+				<FloatingActionButton onClick={this.handleClick} secondary={true} mini={true}>+</FloatingActionButton>
+			</div>
+		);
+	},
+})
+
+var TodoList = React.createClass({
+
+	removeNode(nodeId) {
+		this.props.removeNode(nodeId)
+		return
+	},
+
+	toggleComplete(nodeId) {
+		this.props.toggleComplete(nodeId)
+		return
+	},
+
+	render() {
+		var listNodes = this.props.todos.map((listItem) => {
+			return (
+				<TodoItem key={listItem.id} nodeId={listItem.id} task={listItem.task} complete={listItem.complete} removeNode={this.removeNode} toggleComplete={this.toggleComplete} />
+			)
+		},this)
+		return (
+			<ul>
+				{listNodes}
+			</ul>
+		)
+	},
+})
+
+var TodoItem = React.createClass({
+
+	removeNode(e) {
+		e.preventDefault()
+		this.props.removeNode(this.props.nodeId)
+		return
+	},
+
+	toggleComplete(e) {
+		e.preventDefault()
+		this.props.toggleComplete(this.props.nodeId)
+		return
+	},
+	
+	render() {
+		let isLineThrough = ''
+		let isCheck = ''
+		if (this.props.complete === 'true') {
+			isLineThrough = {'text-decoration':'line-through'}
+			isCheck = 'checked'
+		}else{
+			isLineThrough = {'text-decoration':'none'}
+		}
+
+		return (
+			<div>
+				<Card>
+			 		<Card style={AppStyle.cardStyling}>
+						<Checkbox defaultChecked={isCheck} onCheck={this.toggleComplete} labelStyle={isLineThrough} label={this.props.task} />
+			 		</Card>
+			 		<Card style={AppStyle.cardStyling}>
+						<button type="button" onClick={this.removeNode} style={{backgroundColor:'salmon'}}>&#xff38;</button>
+			 		</Card>
+				</Card>
+			</div>
+		)
+	},
+})
+
+ReactDOM.render(<App />, document.getElementById('root'));
---
+++ src/styles/App.js
@@ -0,0 +1,5 @@
+export default {
+	cardStyling: {
+		display: 'inline-block',
+	},
+}
